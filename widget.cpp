bd0451d78f375fce30b53115b79741e141e0c353
Bo Shang <cnpcshangbo@gmail.com>
2022-05-03 19:09:05 +0000

    birds ui for assisted hovering and auto-clamping

--- widget.cpp
+++ widget.cpp
@@ -26,41 +26,71 @@ void Widget::on_pushButton_roscore_clicked()
 {
     cout << "clicked." << endl;
 //    cout << ui->lineEdit_vicon->text().toStdString() << endl;
-    string viconStart("gnome-terminal -x bash -c \". ~/catkin_ws/devel/setup.bash; roslaunch vicon_bridge vicon.launch datastream_hostport:=\\\"");
+    string cmdStart("gnome-terminal -x bash -c \". ~/catkin_ws/devel/setup.bash; roscore\"");
 //    string viconIP(ui->lineEdit_vicon->text().toStdString());
-    string viconEnd("\\\"; echo Finished roslaunch mavros apm2.launch; bash\"");
-    string viconCmd = viconStart + viconEnd;
-    cout << viconCmd << endl;
-    system(viconCmd.c_str());
+    string cmdEnd("\"; echo Finished roscore; bash\"");
+    string Cmd = cmdStart + cmdEnd;
+    cout << Cmd << endl;
+    system(Cmd.c_str());
 }
 
 
 void Widget::on_pushButton_vicon_bridge_clicked()
 {
     cout << "clicked." << endl;
+    string cmdStart("gnome-terminal -x bash -c \". ~/catkin_ws/devel/setup.bash; . ~/bash/vicon_bridge.bash");
+//    string viconIP(ui->lineEdit_vicon->text().toStdString());
+    string cmdEnd("; echo Finished . ~/vicon_bridge.bash; bash\"");
+    string Cmd = cmdStart + cmdEnd;
+    cout << Cmd << endl;
+    system(Cmd.c_str());
 }
 
 
 void Widget::on_pushButton_vicon2mavros_clicked()
 {
-        cout << "clicked." << endl;
+    cout << "clicked." << endl;
+    string cmdStart("gnome-terminal -x bash -c \". ~/catkin_ws/devel/setup.bash; . ~/bash/vicon2mavros.bash");
+//    string viconIP(ui->lineEdit_vicon->text().toStdString());
+    string cmdEnd("; echo Finished . ~/vicon2mavros.bash; bash\"");
+    string Cmd = cmdStart + cmdEnd;
+    cout << Cmd << endl;
+    system(Cmd.c_str());
 }
 
 
 void Widget::on_pushButton_mavros_clicked()
 {
-        cout << "clicked." << endl;
+    cout << "clicked." << endl;
+    string cmdStart("gnome-terminal -x bash -c \". ~/catkin_ws/devel/setup.bash; . ~/bash/mavros.bash");
+//    string viconIP(ui->lineEdit_vicon->text().toStdString());
+    string cmdEnd("; echo Finished . ~/mavros.bash; bash\"");
+    string Cmd = cmdStart + cmdEnd;
+    cout << Cmd << endl;
+    system(Cmd.c_str());
 }
 
 
 void Widget::on_pushButton_girder_detection_clicked()
 {
-        cout << "clicked." << endl;
+    cout << "clicked." << endl;
+    string cmdStart("gnome-terminal -x bash -c \". ~/catkin_ws/devel/setup.bash; . ~/bash/girder-detection.bash");
+//    string viconIP(ui->lineEdit_vicon->text().toStdString());
+    string cmdEnd("; echo Finished . ~/girder-detection.bash; bash\"");
+    string Cmd = cmdStart + cmdEnd;
+    cout << Cmd << endl;
+    system(Cmd.c_str());
 }
 
 
 void Widget::on_pushButton_distance2serial_clicked()
 {
-        cout << "clicked." << endl;
+    cout << "clicked." << endl;
+    string cmdStart("gnome-terminal -x bash -c \". ~/catkin_ws/devel/setup.bash; . ~/bash/distance2serial.bash");
+//    string viconIP(ui->lineEdit_vicon->text().toStdString());
+    string cmdEnd("; echo Finished . ~/distance2serial.bash; bash\"");
+    string Cmd = cmdStart + cmdEnd;
+    cout << Cmd << endl;
+    system(Cmd.c_str());
 }
 
